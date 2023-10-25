c55d2caba2dc4116c3f1c050522bce180e68d793
Justin Roberts <jrr4n@virginia.edu>
2023-10-24 22:16:37 -0400

    added new files. have error when try install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/cvillejustin/M09_Exercise_Repo',
+    author='Justin Roberts',
+    author_email='test_email@gmail.com'
+    license='MIT',
+    packages=['demo']
+)
